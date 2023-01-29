b350f07eb6889cfed6e4c4dd8fb61d7cc6164b93
ibra.malahat <ibramila2002@gmail.com>
2023-01-29 19:17:39 +0400

    fetch data

---
+++ js.js
@@ -0,0 +1,20 @@
+
+let tbody = document.getElementById("tbody")
+console.log(tbody);
+let count = 0;
+
+fetch('https://northwind.vercel.app/api/products')
+    .then((response) => response.json())
+    .then((json) => {
+        json.forEach(element => {
+            count++
+            tbody.innerHTML += `<tr>
+        <th scope="row">${count}</th>
+        <td>${element.name}</td>
+        <td>${element.unitPrice}</td>
+        <td style="color: ${element.category?.name == undefined ? "red" : "black"}" > ${(element.category?.name == undefined) ? "category not found" : element.category.name}</td>
+        <td>${(element.discontinued == true) ? "YES" : "FALSE"}</td>
+      </tr>`
+        });
+
+    });     
